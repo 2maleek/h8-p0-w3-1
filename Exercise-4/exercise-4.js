438a60eac9939731f37089320e7b1d5a64e1342b
akbarakma <akbarajo1234@gmail.com>
2019-12-17 15:16:24 +0700

    update 4

---
+++ Exercise-4/exercise-4.js
@@ -0,0 +1,14 @@
+var input = ["0001", "Roman Alamsyah ", "Bandar Lampung", "21/05/1989", "Membaca"];
+dataHandling2(input){
+    input.splice(1, 3, )
+}
+
+/**
+ * keluaran yang diharapkan (pada console)
+ *
+ * ["0001", "Roman Alamsyah Elsharawy", "Provinsi Bandar Lampung", "21/05/1989", "Pria", "SMA Internasional Metro"]
+ * Mei
+ * ["1989", "21", "05"]
+ * 21-05-1989
+ * Roman Alamsyah
+ */httphttps://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_splicehttps://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_splicehttps://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_splicehttps://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_splicehttps://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_splices://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_splice
