a0b32b841d2cd1df70301db7121b272eb51216f0
akbarakma <akbarajo1234@gmail.com>
2019-12-27 19:29:09 +0700

    selesain ntar

--- Exercise-14/exercise-14.js
+++ Exercise-14/exercise-14.js
@@ -1,12 +1,32 @@
 function mengelompokkanAngka(arr) {
     
-    var hasil = [][];
+    var hasil = [];
+    var genap = [];
+    var ganjil = [];
+    var kelipatan = [];
     for(var i = 0 ; i < arr.length ; i ++){
         if(arr[i] % 2 === 0){
-            
+            genap.push(arr[i]);
+        }
+        else if(arr[i] % 2 === 1){
+            ganjil.push(arr[i]);
+        }
+        else if(arr[i] % 3 === 0){
+            kelipatan.push(arr[i]);
         }
     }
 
+    for(var i = 0 ; i < genap.length ; i ++){
+        hasil[0][i] = genap[i];
+    }
+    for(var i = 0 ; i < ganjil.length ; i ++){
+        hasil[1][i] = ganjil[i];
+    }
+    for(var i = 0 ; i < kelipatan.length ; i ++){
+        hasil[2][i] = kelipatan[i];
+    }
+
+    return hasil;
 
   }
   
