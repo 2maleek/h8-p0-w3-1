271e9515c209f9801cb806c668f4e39a4b42d93b
akbarakma <akbarajo1234@gmail.com>
2020-01-03 15:41:35 +0700

    selese

--- Exercise-15/exercise-15.js
+++ Exercise-15/exercise-15.js
@@ -1,20 +1,44 @@
 function groupAnimals(animals) {
   
-    var hasil = [];
-    var abjad = 'abcdefghijklmnopqrstuvwxyz';
-    for(var i = 0 ; i < abjad.length ; i ++){
-        var sementara = [];
-        for(var j = 0 ; j < animals.length ; j ++){
-            if(abjad[i] === animals[j][0]){
-                sementara.push(animals[j]);
+    var sort = animals;
+    for(var i = 0 ; i < sort.length ; i ++){
+        for(var j = 0 ; j < sort.length ; j ++){
+            if(sort[j] > sort[j+1]){
+                var tmp = sort[j];
+                sort[j] = sort[j+1];
+                sort[j+1] = tmp;
             }
         }
-        if(sementara.length > 0){
+    }
+
+    var hasil = [];
+    var tampungHuruf = [];
+    var sementara = [];
+    var huruf = sort[0][0];
+    var check = true;
+    for(var i = 0 ; i < sort.length ; i ++){
+        for(var j = 0 ; j < tampungHuruf.length ; j ++){
+            if(sort[i][0] === tampungHuruf[j]){
+                check = false;
+            }
+        }
+        if(check === true){
+            huruf = sort[i][0];
+            tampungHuruf.push(huruf);
+            for(var k = 0 ; k < sort.length ; k ++){
+                if(sort[k][0] === huruf){
+                    sementara.push(sort[k]);
+                }
+            }
+            
             hasil.push(sementara);
         }
+        
+        sementara = [];
+        check = true;
     }
-    return hasil;
 
+    return hasil;
 }
 
 // TEST CASES
